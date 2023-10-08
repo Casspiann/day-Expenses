157dd61e4acb25051781be975f39a59e6af42ff8
Casspiann <tarunrana4221>
2023-10-08 15:20:39 +0530

    addLogin

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.post('/users/login', async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Retrieve the user from the database by email
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Compare the provided password with the password stored in the database (plaintext comparison)
+    if (password === user.password) {
+      res.status(200).json({ message: 'Successfully Logged In' });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred' });
+  }
+});
+
 app.post('/users/signup', async (req, res, next) => {
   try {
     let name = req.body.name; // Lowercase 'name' variable
@@ -32,7 +57,7 @@ app.post('/users/signup', async (req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 sequelize.sync().then((result)=>{
-  console.log(result);
+ // console.log(result);
   app.listen(4000, () => {
     console.log('Server is running on port 4000');
   });
