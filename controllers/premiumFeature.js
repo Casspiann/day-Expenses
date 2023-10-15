5779b213cd7701b099bd0e829b06d5d7e0478184
Casspiann <tarunrana4221>
2023-10-16 01:04:56 +0530

    add leftouterjoin using sequelize

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -8,11 +8,31 @@ const sequelize = require('../util/database');
 
 exports.getUserLeaderboard = async(req,res,next)=>{
     try{
-        const users = await User.findAll();
-        const expenses = await Expense.findAll();
-        const userAggegrateExpense = {};
+        const leaderBoardOfUser = await User.findAll({
+          attributes:['id','name',
+          [sequelize.fn('sum', sequelize.cast(sequelize.col('expenses.expenceAmmount'), 'DECIMAL')), 'totalCost']
+        ],
+        include:[
+          {
+            model:Expense,
+            attributes:[]
+          }
+        ],
+        group:['user.id'],
+        order:[[sequelize.col('totalCost'),'DESC']]
 
-        expenses.forEach(expense => {
+        });
+       /* const expenses = await Expense.findAll({
+          attributes: [
+            'userId',
+            [sequelize.fn('sum', sequelize.cast(sequelize.col('expenses.expenceAmmount'), 'DECIMAL')), 'totalCost']
+          ],
+          group: ['userId']
+        });
+        const userAggegrateExpense = {};
+        console.log(expenses);
+
+       /* expenses.forEach(expense => {
             const expenseAmount = parseInt(expense.expenceAmmount, 10); // Convert the string to an integer
             if (!isNaN(expenseAmount)) {
               if (userAggegrateExpense[expense.userId]) {
@@ -30,8 +50,8 @@ exports.getUserLeaderboard = async(req,res,next)=>{
             userLeaderBoardDetails.push({ name: user.name, totalCost: userAggegrateExpense[user.id] || 0 });
         })
         userLeaderBoardDetails.sort((a, b) => b.totalCost - a.totalCost);
-        console.log(userLeaderBoardDetails);
-        res.status(200).json({userLeaderBoardDetails});
+        console.log(userLeaderBoardDetails);*/
+        res.status(200).json({leaderBoardOfUser});
     }
     catch(err){
         console.log(err);
